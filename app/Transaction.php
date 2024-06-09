59af09765fc2a81083cdd94bcb8640aa518c14d7
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-06-09 13:46:46 +0300

    Add transaction class

---
+++ app/Transaction.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+
+class Transaction
+{
+    private int $amountIn;
+    private string $currencyIn;
+    private string $type;
+    private int $amountOut;
+    private string $currencyOut;
+    private ?Carbon $createdAt;
+
+    public function __construct(
+        int $amountIn,
+        string $currencyIn,
+        string $type,
+        int $amountOut,
+        string $currencyOut,
+        ?Carbon $createdAt
+    ) {
+        $this->amountIn = $amountIn;
+        $this->currencyIn = $currencyIn;
+        $this->type = $type;
+        $this->amountOut = $amountOut;
+        $this->currencyOut = $currencyOut;
+        $this->createdAt = $createdAt ? Carbon::parse($createdAt) : Carbon::now("UTC");
+    }
+
+    public function amountIn(): int
+    {
+        return $this->amountIn;
+    }
+
+    public function currencyIn(): string
+    {
+        return $this->currencyIn;
+    }
+
+    public function amountOut(): int
+    {
+        return $this->amountOut;
+    }
+
+    public function currencyOut(): string
+    {
+        return $this->currencyOut;
+    }
+
+    public function type(): string
+
+    public function type(): string
+    {
+        return $this->type;
+    }
+
+    public function createdAt(): ?Carbon
+    {
+        return $this->createdAt;
+    }
+
+}
